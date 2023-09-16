7580bdea6ad0aa324afe1fa014be6b7e83c6b218
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-16 15:03:41 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+// Macro to determine if a character is uppercase
+
 #include <stdio.h>
 
+#define IS_UPPER_CASE(x) (x) >= 'A' && (x) <= 'Z' ? 1 : 0
+
 int main(void) {
-  printf("Hello World\n");
+
+  printf ("%d\n", IS_UPPER_CASE('F')); // 1
+  printf ("%d\n", IS_UPPER_CASE('f')); // 0
+  
   return 0;
 }
